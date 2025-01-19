c124d3feae87a8fa836156347a542ab18e88e1ac
katiasveloso <katisv@gmail.com>
2025-01-19 20:51:12 -0300

    atualizar arquivo exercicio fixacao

--- curso_programcao/src/curso_programcao/exercicio_fixacao.java
+++ curso_programcao/src/curso_programcao/exercicio_fixacao.java
@@ -13,6 +13,19 @@
  * double price2 = 650.50;
  * double measure = 53.234567; 
   
+Em seguida, usando os valores das variáveis, produza a
+seguinte saída na tela do console:
+
+Products:
+Computer, which price is $ 2100,00
+Office desk, which price is $ 650,50
+
+Record: 30 years old, code 5290 and gender: F
+
+Measue with eight decimal places: 53,23456700
+Rouded (three decimal places): 53,235
+US decimal point: 53.235
+  
  *************************************************************************/
 
 
